bdd6ef43e64f0aa1f22df01d3e076ca9625ce8fb
sn4ke-laptop <michael.retamozo@gmail.com>
2022-07-12 17:40:39 +0200

    test: customCSS giscus 2

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -51,7 +51,7 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: '',
+      theme: 'custom',
       // Place the comment box above the comments. options: bottom, top
       inputPosition: 'bottom',
       // Choose the language giscus will be displayed in. options: en, es, zh-CN, zh-TW, ko, ja etc
@@ -61,7 +61,7 @@ const siteMetadata = {
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '../css/custom-giscus.css',
+      themeURL: 'https://giscus.app/themes/custom_example.css',
     },
     utterancesConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
