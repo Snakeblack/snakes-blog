baa4bbba6a51c8309cb6261af48d12c7400d49c5
sn4ke-desktop <michael.retamozo@gmail.com>
2022-07-14 04:58:10 +0200

    update: hotfixs 🔥🔧

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
         </div>
-        <div className="mb-2 flex space-x-2 text-[11px] text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 flex space-x-2 text-[11px] text-gray-500 dark:text-gray-400 sm:text-sm">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
