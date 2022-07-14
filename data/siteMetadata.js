c76c57693598c4def30771b6f68eb8d36e1d7b2c
sn4ke-desktop <michael.retamozo@gmail.com>
2022-07-14 04:53:41 +0200

    update: hotfixs 🔥🔧

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -29,16 +29,10 @@ const siteMetadata = {
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-    // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
   comment: {
-    // If you want to use a commenting system other than giscus you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: 'giscus',
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
@@ -64,22 +58,6 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
-    // utterancesConfig: {
-    //   // Visit the link below, and follow the steps in the 'configuration' section
-    //   // https://utteranc.es/
-    //   repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-    //   issueTerm: '', // supported options: pathname, url, title
-    //   label: '', // label (optional): Comment 💬
-    //   // theme example: github-light, github-dark, preferred-color-scheme
-    //   // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-    //   theme: '',
-    //   // theme when dark mode
-    //   darkTheme: '',
-    // },
-    // disqusConfig: {
-    //   // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-    //   shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-    // },
   },
 }
 
